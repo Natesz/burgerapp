d44e8303da45aa2e28d15680c87fabb9e2fb1d7d
Natesz <natesz777@yahoo.com>
2020-04-12 19:53:58 +0200

    init commit

---
+++ build/precache-manifest.e370b4446567d9a422ddafe59f184d34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37aa052d22a01af8097c7d7b9daf1169",
+    "url": "/burgerapp/index.html"
+  },
+  {
+    "revision": "80ca7f63585c612f6ad7",
+    "url": "/burgerapp/static/css/main.a753c83e.chunk.css"
+  },
+  {
+    "revision": "ca6cff588b95554a4d31",
+    "url": "/burgerapp/static/js/2.d13c46f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burgerapp/static/js/2.d13c46f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80ca7f63585c612f6ad7",
+    "url": "/burgerapp/static/js/main.76396b71.chunk.js"
+  },
+  {
+    "revision": "ecbf29f8c2ddb752355e",
+    "url": "/burgerapp/static/js/runtime-main.88ce74f8.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burgerapp/static/media/original.b8503d26.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b26e4bdd45183092e206339270cc260e.js"
+  "/burgerapp/precache-manifest.e370b4446567d9a422ddafe59f184d34.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/burgerapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
